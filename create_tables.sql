bd48da0f718b688259249f59206bc65c0cdb6bc3
Nicolai1997 <nico7_messi10@yahoo.it>
2019-10-08 12:17:16 +0200

    Ho creato nuove tabelle

--- create_tables.sql
+++ create_tables.sql
@@ -41,7 +41,59 @@ CREATE TABLE Prodotto (
 	CHECK (Prezzo > 0)
 ); 
 
-CREATE TABLE CodiceCarrello ( 
+CREATE TABLE Categoria ( 
 	CodiceCategoria INTEGER primary key,
 	NomeCategoria VARCHAR(255) not null
 );
+
+CREATE TABLE Carta(
+	NumeroCarta INTEGER primary key,
+	Scadenza date not null,
+	CodiceNumericoDiSicurezza INTEGER not null
+);
+
+CREATE TABLE Carello(
+	CodiceCarrello INTEGER not null,
+	PrezzoTotale DECIMAL not null
+);
+
+CREATE TABLE ProdottoCarrello(
+	CodiceCarrello INTEGER primary key not null, 
+	CodiceProdotto INTEGER not null references Prodotto (CodiceProdotto), 
+	quantità INTEGER not null
+);
+
+CREATE TABLE MetodoDiPagamento(
+	CF VARCHAR(16) not null unique,
+	NumeroCarta INTEGER not null unique,
+	PRIMARY KEY(CF,NumeroCarta)
+);
+
+CREATE TABLE Furgone(
+	Targa VARCHAR(7) not null PRIMARY KEY, 
+	MetodoDiCarburazione VARCHAR(20), 
+	ScadenzaAssicurazione date not null
+);
+
+CREATE TABLE Corriere(
+	Targa VARCHAR(7) not null unique references Furgone(Targa),
+	Data date not null unique,
+	CodiceFiscale VARCHAR(16) not null, 
+	PRIMARY KEY(Targa,Data)
+);
+
+CREATE TABLE Spedizione(
+CodiceSpedizione INTEGER not null PRIMARY KEY,
+Targa VARCHAR(7) not null references Corriere(Targa),
+Data date not null references Corriere(Data),
+CodiceSpedizioneRiprovata date not null references Spedizione
+);
+
+CREATE TABLE Acquisto(
+	CodiceAcquisto INTEGER not null PRIMARY KEY,
+	CF VARCHAR(16) not null references MetodoDiPagamento(CF),
+	NumeroCarta INTEGER not null references MetodoDiPagamento(NumeroCarta),
+	CodiceSpedizione INTEGER not null references Spedizione(CodiceSpedizione)
+);
+
+
